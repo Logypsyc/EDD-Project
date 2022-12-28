bb1b0fbbdd1131cc08c652f56b5b081295a2cffc
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-12-27 22:02:15 -0500

    Update and rename testCamera.py to camera.py

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
-import cv2
+  GNU nano 5.4                                                              camera.py                                                                       import cv2
 
-cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("le camera POV")
 
