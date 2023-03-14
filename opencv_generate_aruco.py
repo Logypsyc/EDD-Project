e2e41c408c59df0cc7e61a78b68563c5ffef3f60
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2023-03-14 00:08:02 -0400

    changed from 300 px to 576 px

--- opencv_generate_aruco.py
+++ opencv_generate_aruco.py
@@ -6,13 +6,9 @@ import sys
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", required=True,
-	help="path to output image containing ArUCo tag")
-ap.add_argument("-i", "--id", type=int, required=True,
-	help="ID of ArUCo tag to generate")
-ap.add_argument("-t", "--type", type=str,
-	default="DICT_ARUCO_ORIGINAL",
-	help="type of ArUCo tag to generate")
+ap.add_argument("-o", "--output", required=True, help="path to output image containing ArUCo tag")
+ap.add_argument("-i", "--id", type=int, required=True, help="ID of ArUCo tag to generate")
+ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="type of ArUCo tag to generate")
 args = vars(ap.parse_args())
 
 # define names of each possible ArUco tag OpenCV supports
@@ -42,8 +38,7 @@ ARUCO_DICT = {
 
 # verify that the supplied ArUCo tag exists and is supported by OpenCV
 if ARUCO_DICT.get(args["type"], None) is None:
-	print("[INFO] ArUCo tag of '{}' is not supported".format(
-		args["type"]))
+	print("[INFO] ArUCo tag of '{}' is not supported".format(args["type"]))
 	sys.exit(0)
 
 # load the ArUCo dictionary
@@ -51,8 +46,8 @@ arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
 
 # allocate memory for the output ArUCo tag and then draw the ArUCo tag on the output image
 print("[INFO] generating ArUCo tag type '{}' with ID '{}'".format(args["type"], args["id"]))
-tag = np.zeros((300, 300, 1), dtype="uint8")
-cv2.aruco.drawMarker(arucoDict, args["id"], 300, tag, 1)
+tag = np.zeros((576, 576, 1), dtype="uint8")
+cv2.aruco.drawMarker(arucoDict, args["id"], 576, tag, 1)
 
 # write the generated ArUCo tag to disk and then display it to our screen
 cv2.imshow("ArUCo Tag", tag)
@@ -60,4 +55,3 @@ k = cv2.waitKey(0)
 if k == 27:
 	cv2.imwrite(args["output"], tag)
 	cv2.destroyAllWindows()
-
