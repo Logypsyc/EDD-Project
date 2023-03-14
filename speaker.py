11c828fbbf2168c95d6388b19bb219d1f9eaadbf
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2023-03-14 08:20:51 -0400

    Add files via upload

---
+++ speaker.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+import termios
+pygame.mixer.init()
+pygame.mixer.music.load("/home/pi/Documents/turnRight.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+	continue
+
